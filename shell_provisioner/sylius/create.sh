a1ad0b4ad1bc22e3dcc88a730f1d374178bfa50b
Tony Destro <tony.destro@gmail.com>
2018-02-06 20:07:34 -0500

    first commit

--- shell_provisioner/sylius/create.sh
+++ shell_provisioner/sylius/create.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /var/www/sites
-
-composer create-project --no-progress -n sylius/sylius-standard ./sylius
+ [ "$(ls -A ./sylius)" ] && echo "Directory sylius is not empty." || { composer create-project --no-progress -n sylius/sylius-standard ./sylius; rm ./sylius/.gitignore; }
