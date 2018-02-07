36e303e3ecc74f2e1f5cd52196b5ff67f5fd8df0
Tony Destro <tony.destro@gmail.com>
2018-02-06 21:28:31 -0500

    get files for sylius from own fork

--- shell_provisioner/sylius/create.sh
+++ shell_provisioner/sylius/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /var/www/sites
- [ "$(ls -A ./sylius)" ] && echo "Directory sylius is not empty." || composer create-project --no-progress -n sylius/sylius-standard ./sylius
+ [ "$(ls -A ./sylius)" ] && echo "Directory sylius is not empty." || composer create-project --no-progress -n tdestro/sylius-standard ./sylius
