20d47e7005c1e630aed7540c7c6ed960411336c9
Tony Destro <tony.destro@gmail.com>
2018-02-06 21:01:04 -0500

    remove delete git

--- shell_provisioner/sylius/create.sh
+++ shell_provisioner/sylius/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /var/www/sites
- [ "$(ls -A ./sylius)" ] && echo "Directory sylius is not empty." || { composer create-project --no-progress -n sylius/sylius-standard ./sylius; rm ./sylius/.gitignore; }
+ [ "$(ls -A ./sylius)" ] && echo "Directory sylius is not empty." || composer create-project --no-progress -n sylius/sylius-standard ./sylius
