1d99ad130aa9564afd28a7bc745cac9f52e4f3b1
Tony Destro <tony.destro@gmail.com>
2018-02-25 00:27:46 -0500

    updates to get this working again

--- shell_provisioner/module/php.sh
+++ shell_provisioner/module/php.sh
@@ -10,11 +10,12 @@ sudo apt update
 apt-get -y install php7.1-cli php7.1-fpm php7.1-dev php7.1-curl php7.1-intl \
     php7.1-mysql php7.1-sqlite3 php7.1-gd php7.1-mbstring php7.1-xml
 
-sed -i 's/;date.timezone.*/date.timezone = Europe\/Brussels/' /etc/php/7.1/fpm/php.ini
-sed -i 's/;date.timezone.*/date.timezone = Europe\/Brussels/' /etc/php/7.1/cli/php.ini
+sed -i 's/;date.timezone.*/date.timezone = UTC/' /etc/php/7.1/fpm/php.ini
+sed -i 's/;date.timezone.*/date.timezone = UTC/' /etc/php/7.1/cli/php.ini
 sed -i 's/^user = www-data/user = vagrant/' /etc/php/7.1/fpm/pool.d/www.conf
 sed -i 's/^group = www-data/group = vagrant/' /etc/php/7.1/fpm/pool.d/www.conf
-
+sed -i -r -e 's/display_errors = Off/display_errors = On/g' /etc/php/7.1/fpm/php.ini
+sed -i -r -e 's/display_errors = Off/display_errors = On/g' /etc/php/7.1/cli/php.ini
 service php7.1-fpm restart
 
 # composer
--- shell_provisioner/sylius/create.sh
+++ shell_provisioner/sylius/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /var/www/sites
- [ "$(ls -A ./sylius)" ] &&echo "Directory sylius is not empty." ||{ git clone https://github.com/tdestro/Sylius-Standard.git ./sylius; cd ./sylius; composer install; }
+ [ "$(ls -A ./sylius)" ] &&echo "Directory sylius is not empty." ||{ git clone https://github.com/tdestro/Sylius.git ./sylius; cd ./sylius; composer install; }
